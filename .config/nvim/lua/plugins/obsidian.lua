160458c17cb3ce6b7de441b059676d42585723cf
lewis <lewisguignard@gmail.com>
2026-05-29 21:46:02 -0700

    obsidian changes

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,63 +1,85 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "saghen/blink.compat",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "caremaze",
+          path = "~/Obsidian/caremaze",
+        },
+        {
+          name = "obsidian-vault-template",
+          path = "~/Obsidian/obsidian-vault-template",
+        },
+        {
+          name = "second-brain",
+          path = "~/Obsidian/second-brain",
+        },
+      },
 
-    -- see below for full list of optional dependencies 👇
+      daily_notes = {
+        folder = "daily",
+      },
+
+      ui = { enable = false },
+      mappings = {
+        ["<leader>ch"] = {
+          action = function()
+            local line = vim.api.nvim_get_current_line()
+            local new_line
+            if line:match("%[x%]") then
+              new_line = line:gsub("%[x%]", "[ ]", 1)
+            elseif line:match("%[.%]") then
+              new_line = line:gsub("%[.%]", "[x]", 1)
+            end
+            if new_line then
+              vim.api.nvim_set_current_line(new_line)
+            end
+          end,
+          opts = { buffer = true },
+        },
+      },
+      checkboxes = {
+        [" "] = { order = 1, char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { order = 2, char = "", hl_group = "ObsidianDone" },
+      },
+    },
   },
-  opts = {
-    workspaces = {
-      {
-        name = "caremaze",
-        path = "~/Obsidian/caremaze",
-      },
-      {
-        name = "obsidian-vault-template",
-        path = "~/Obsidian/obsidian-vault-template",
-      },
-      {
-        name = "second-brain",
-        path = "~/Obsidian/second-brain",
-      },
-    },
 
-    daily_notes = {
-      folder = "daily",
-    },
-
-    ui = { enable = false },
-    mappings = {
-      ["<leader>ch"] = {
-        action = function()
-          local line = vim.api.nvim_get_current_line()
-          local new_line
-          if line:match("%[x%]") then
-            new_line = line:gsub("%[x%]", "[ ]", 1)
-          elseif line:match("%[.%]") then
-            new_line = line:gsub("%[.%]", "[x]", 1)
-          end
-          if new_line then
-            vim.api.nvim_set_current_line(new_line)
-          end
-        end,
-        opts = { buffer = true },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "saghen/blink.compat" },
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = {},
+        ["<C-p>"] = {},
       },
-    },
-    checkboxes = {
-      [" "] = { order = 1, char = "󰄱", hl_group = "ObsidianTodo" },
-      ["x"] = { order = 2, char = "", hl_group = "ObsidianDone" },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "obsidian", "obsidian_new", "obsidian_tags" },
+        providers = {
+          obsidian = { name = "obsidian", module = "blink.compat.source" },
+          obsidian_new = { name = "obsidian_new", module = "blink.compat.source" },
+          obsidian_tags = { name = "obsidian_tags", module = "blink.compat.source" },
+          buffer = {
+            transform_items = function(_, items)
+              return vim.tbl_filter(function(item)
+                -- drop long hash-like tokens (excalidraw/base64 noise)
+                return not (#item.label > 20 and item.label:match("^[A-Za-z0-9+/=_-]+$"))
+              end, items)
+            end,
+          },
+        },
+      },
     },
   },
 }
